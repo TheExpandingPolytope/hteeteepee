043ac4b1d6ff07d8a7644167e87fab6b903f4d7e
Jesse Engerman <jesseengerman@hotmail.com>
2020-06-10 12:02:22 -0500

    tcp and http version 1.1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,74 @@
 use log::{error, info};
-use std::TcpListener;
+use std::error::Error;
+use std::net::{TcpListener, TcpStream};
+use std::path::Path;
+use std::{fmt, fs};
+use std::io::{Read, Write};
 
-fn handle_connection(){
+struct Request<'a> {
+    method: &'a str,
+    uri: &'a Path,
+    httpVersion: &'a str,
+}
 
+impl<'a> fmt::Display for Request<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} {} {}\r\n",
+            self.method,
+            self.uri.display(),
+            self.httpVersion,
+        )
+    }
+}
+
+fn parse_line(req: &str) -> Result<Request, Box<dyn Error>> {
+    let mut parts = req.split_whitespace();
+    let method = parts.next().ok_or("no method specified")?;
+    if method != "GET" {
+        Err("not supported")?;
+    }
+
+    let uri = Path::new(parts.next().ok_or("no uri specified")?);
+    let normUri = uri.to_str().expect("unicode is invalid");
+    const root: &str = "home/";
+    if !Path::new(&format!("{}{}", root, normUri)).exists() {
+        Err("resource does not exist")?;
+    }
+
+    let httpVersion = parts.next().ok_or("no version specified")?;
+    if httpVersion != "HTTP/1.1" {
+        Err("ONLY HTTP 1.1 MAN")?;
+    }
+
+    Ok(Request {
+        method,
+        uri,
+        httpVersion,
+    })
+}
+
+fn onConnection(mut tcp_stream: TcpStream) -> Result<(), Box<dyn Error>>{
+    let mut buffer = [0; 512];
+    tcp_stream.read(&mut buffer).unwrap(); //read stream data onto buffer
+    let req = String::from_utf8_lossy(&buffer[..]);
+    let line = req.lines().next().unwrap();
+
+    match parse_line(&line) {
+        Ok(req) => {
+            info!("Request: {}", &req);
+
+            let contents = fs::read_to_string("index.html").unwrap();
+            let response = format!("{}{}", "HTTP/1.1 200 OK\r\n\r\n", contents);
+
+            tcp_stream.write(response.as_bytes()).unwrap();
+            tcp_stream.flush().unwrap();
+        }
+        Err(e) => error!("not formartted: {}", e),
+    }
+
+    Ok(())
 }
 
 fn main() {
@@ -11,12 +77,12 @@ fn main() {
 
     let ip = "127.0.0.1:8000";
 
-    let listener = TcpListener.bind(ip).expect("failed to start");
+    let listener = TcpListener::bind(ip).expect("failed to start");
     info!("server started on : {} {}", "http://", ip);
 
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => match handle_connection(stream) {
+            Ok(stream) => match onConnection(stream) {
                 Ok(_) => (),
                 Err(e) => error!("Error handling connection: {}", e),
             },
